d2e8623bf0ce1594785f80ae16d0f41608693392
Javier <javierLhernandez@yahoo.com>
2026-07-23 21:45:53 -0400

    pseudo code

---
+++ main.js
@@ -0,0 +1,12 @@
+//have object with key:values consisting of ports as the key and protocols abbreviation + name as the value
+//the last key/value pair is add to a new array?
+//random item in that array is presented. The protocol abbreviation is presented.
+//Enter the port for that protocol correctly
+    //if all items in that array(maybe object) have been answered correctly, 
+    //add a new key/value from original to second array, and present that new item as question.
+    //if correct, repeat.
+        //if incorrect, present the right answer for 3 seconds. 
+        //Then, start from the beginning of the 2nd array (or do random presentation), 
+        //keep that new item in the array.
+        //once presented, if answered incorrectly, repeat process until it is answere correctly.
+        //If answered correcty, add new item, etc until original object is empty.
